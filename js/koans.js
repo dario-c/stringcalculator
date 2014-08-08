ae1bd476ffb1beb538307a90bde93533bba97b86
catburston <cat1788@gmail.com>
2014-08-08 16:56:57 +0200

    created failing test for addition

--- js/koans.js
+++ js/koans.js
@@ -1,9 +1,11 @@
 
-describe("The Awesome Transformer(tm)", function(){
-  it("Return a number when passed an number as text", function() {
-    expect(transformTextToNumber("10")).toBe(10);
+describe("The Awesome Calculator(tm)", function(){
+  it("returns a number when passed an number as text", function() {
+    expect(calculates("10")).toBe(10);
   });
 
- 
+  it("returns the product of two numbers passed as text with an addition operator", function() {
+    expect(calculates("1+1")).toBe(2);
+  });
 
 })
---
+++ js/string-calc.js
@@ -0,0 +1,9 @@
+function calculates(text) {
+  //transformTextToNumber(text);
+  return transformTextToNumber(text);
+}
+
+function transformTextToNumber(text) {
+  var convertedNumber = parseInt(text, 10);
+  return convertedNumber;
+}
