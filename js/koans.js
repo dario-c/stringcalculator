35e0d9b583e56708988f42a499adf14a9d1bae3a
catburston <cat1788@gmail.com>
2014-08-10 17:00:16 +0200

    working calculator - transforms string to number and performs basic math operations

--- js/koans.js
+++ js/koans.js
@@ -1,11 +1,39 @@
 
 describe("The Awesome Calculator(tm)", function(){
   it("returns a number when passed an number as text", function() {
-    expect(calculates("10")).toBe(10);
+    expect(calculator("10")[3]).toBe(10);
+  });
+
+  it("detects the mathematical plus operator passed as string", function() {
+    expect(calculator("10+10")[0]).toBe('+');
+  });
+
+  it("detects the mathematical minus operator passed as string", function() {
+    expect(calculator("10-10")[0]).toBe('-');
+  });
+
+  it("detects the mathematical multiplier operator passed as string", function() {
+    expect(calculator("10*10")[0]).toBe('*');
+  });
+
+  it("detects the mathematical division operator passed as string", function() {
+    expect(calculator("10/10")[0]).toBe('/');
+  });
+
+  it("detects the index of the mathematical operator", function() {
+    expect(calculator("1+1")[1]).toBe(1);
+  });
+
+  it("detects the first number in the string", function() {
+    expect(calculator("15+19")[2]).toBe(15);
+  });
+
+  it("detects the second number in the string", function() {
+    expect(calculator("15+19")[3]).toBe(19);
   });
 
   it("returns the product of two numbers passed as text with an addition operator", function() {
-    expect(calculates("1+1")).toBe(2);
+    expect(calculator("1+1")[4]).toBe(2);
   });
 
 })
--- js/string-calc.js
+++ js/string-calc.js
@@ -1,9 +1,57 @@
-function calculates(text) {
-  //transformTextToNumber(text);
-  return transformTextToNumber(text);
+function calculator(text) {
+  var text = text;
+  // transformTextToNumber(text);
+  findOperators(text);
+  splitStringToNumerals(text, operatorIndex);
+  performMathOperation(firstNumeral, operator, secondNumeral)
+  return [operator, operatorIndex, firstNumeral, secondNumeral, result];
 }
 
+var convertedNumber;
 function transformTextToNumber(text) {
-  var convertedNumber = parseInt(text, 10);
+  convertedNumber = parseInt(text, 10);
   return convertedNumber;
+}
+
+var operator;
+var operatorIndex;
+var mathOperators = ['+', '-', '*', '/'];
+function findOperators(text) {
+  for (var i = 0; i < mathOperators.length; i++) {
+    if (text.indexOf(mathOperators[i]) != -1) {
+      operator = mathOperators[i];
+      operatorIndex = text.indexOf(mathOperators[i]);
+      return [operator, operatorIndex];
+    } else {
+      firstNumeral = transformTextToNumber(firstNumeral);
+    }
+  };
+}
+
+var firstNumeral;
+var secondNumeral;
+function splitStringToNumerals(text, i) {
+  firstNumeral = text.substring(0,i);
+  firstNumeral = transformTextToNumber(firstNumeral);
+  secondNumeral = text.substring(i+1, text.length);
+  secondNumeral = transformTextToNumber(secondNumeral);
+  return [firstNumeral, secondNumeral];
+}
+
+var result;
+function performMathOperation(firstNumeral, operator, secondNumeral) {
+  if (operator = '+') {
+    result = firstNumeral + secondNumeral;
+  } else if (operator = '-') {
+    result = firstNumeral - secondNumeral;
+  } else if (operator = '*') {
+    result = firstNumeral * secondNumeral;
+  } else if (operator = '/') {
+    result = firstNumeral / secondNumeral;
+  } else {
+    console.log("operator not found");
+    result = "operator not found";
+  }
+
+  return result;
 }
